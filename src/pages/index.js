0605a6e47c3796affe7b370130ae0efe13405af1
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-03-12 00:02:55 +0500

    Create SortPopup and pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from './Home'
+export {default as Cart} from './Cart'
+export { default as Categories } from '../components/Categories' 
+export { default as SortPopup } from '../components/SortPopup'
