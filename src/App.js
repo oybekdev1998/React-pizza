1314be7b7fc8b7bf1537f7bd23072be88e90141c
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-03-13 16:12:15 +0500

    Created PropTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import './scss/app.scss';
 
@@ -11,12 +12,20 @@ import { Route, Routes } from "react-router-dom";
 
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json')
+    .then(({data}) => setPizzas(data.pizzas))
+  }, [])
+
+  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/" exact element={<Home items={pizzas} />} />
           <Route path="/cart"  element={<Cart />} />
         </Routes>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 
@@ -16,5 +17,8 @@ const Button = ({onClick, className, children}) => {
   )
 }
 
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired
+}
 export default Button
 
